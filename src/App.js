8384dc2a9c422fab7d243eda3abe93028d81143e
json <json1125@outlook.com>
2021-01-27 12:10:26 +0000

    add router in App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from "react";
+import {
+  BrowserRouteer as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom'
 import MainLayout from "./components/MainLayout";
+import Login from "./components/Login";
 
 import "./styles.css";
 
 const App = () => {
-  return <MainLayout />;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/">
+          <MainLayout />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
