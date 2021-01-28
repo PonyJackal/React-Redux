187d1e82a43cd0e84447ca31f696c0395aa5d71d
json <json1125@outlook.com>
2021-01-28 17:59:13 +0000

    get quotes from fakeAPI

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -4,17 +4,13 @@ import useFetch from '../libs/useFetch'
 const Quote = () => {
   const { data, error, loading } = useFetch('http://localhost:8000/quotes')
 
-  if (error) {
-    return `Error: ${error}`
-  }
+  if (error) return `Error: ${error}`
 
-  if (loading) {
-    return <h3>Loading ...</h3>
-  }
+  if (loading) return <h3>Loading ...</h3>
 
   return (
     <h2>
-      {data.quotes[0].quote} {data.quotes[0].author}
+      {data[0].text} {data[0].author}
     </h2>
   )
 }
