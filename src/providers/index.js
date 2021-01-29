c9dfb416d1cc75f5d32577725574f656b4824f2c
json <json1125@outlook.com>
2021-01-29 14:17:52 +0000

    load QueryClientProvider in AppProviders

--- src/providers/index.js
+++ src/providers/index.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import { AuthProvider } from './AuthProvider'
 
-const AppProviders = ({ children }) => <AuthProvider>{children}</AuthProvider>
+const queryClient = new QueryClient()
+
+const AppProviders = ({ children }) => (
+  <QueryClientProvider client={queryClient}>
+    <AuthProvider>{children}</AuthProvider>
+  </QueryClientProvider>
+)
 
 AppProviders.propTypes = {
-  children: PropTypes.element.isRequired,
+  children: PropTypes.elementType.isRequired,
 }
 export default AppProviders
